924a1ffa1389f0d52e49a94d19c94664a9e8b13d
Chris-Bortel <bortelcc19@gmail.com>
2020-07-13 12:22:07 -0700

    problem 1 is passing

--- code-challenges/challenges-12.test.js
+++ code-challenges/challenges-12.test.js
@@ -10,6 +10,11 @@ E.g. [4,2,7,5,9,2] -> 9
 ------------------------------------------------------------------------------------------------ */
 const maxInArray = (arr) => {
   // Solution code here...
+
+  return arr.reduce((accumulator,value)=>{
+    return Math.max(accumulator,value);
+  },0);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -268,7 +273,7 @@ describe("Testing challenge 4", () => {
   });
 });
 
-describe("Testing challenge 5", () => {
+xdescribe("Testing challenge 5", () => {
   const battleshipData = [
     ["#", " ", "#", " "],
     ["#", " ", "#", " "],
@@ -287,7 +292,7 @@ describe("Testing challenge 5", () => {
   });
 });
 
-describe("Testing challenge 6", () => {
+xdescribe("Testing challenge 6", () => {
   test("It should multiply all the numbers together", () => {
     expect(
       calculateProduct([
@@ -312,20 +317,20 @@ describe("Testing challenge 6", () => {
   });
 });
 
-describe("Testing challenge 7", () => {
+xdescribe("Testing challenge 7", () => {
   test("It should calculate and return the average temperature of the data set", () => {
     expect(averageDailyTemperature(weeklyTemperatures)).toStrictEqual(60.25);
   });
 });
 
-describe("Testing challenge 8", () => {
+xdescribe("Testing challenge 8", () => {
   test("It should return the lowest weekly average temperature within the data set", () => {
     expect(lowestWeeklyAverage(weeklyTemperatures)).toStrictEqual(57);
     expect(lowestWeeklyAverage(lowestWeeklyTemperatureData)).toStrictEqual(46);
   });
 });
 
-describe("Testing challenge 9", () => {
+xdescribe("Testing challenge 9", () => {
   test("It should return the total count for each row", () => {
     let result = excel("1,1,1\n4,4,4\n9,9,9");
     expect(result.length).toStrictEqual(3);
