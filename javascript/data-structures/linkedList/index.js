a8b2bedc4ff04b4e0aa2c6eaa42ece46c2af115a
Chris-Bortel <bortelcc19@gmail.com>
2020-08-24 14:42:27 -0700

    my linked list is working

--- javascript/data-structures/linkedList/index.js
+++ javascript/data-structures/linkedList/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
 const LinkedList = require('./lib/linked-list.js');
+let list = new LinkedList();
+
+list.insert('cool value one');
+list.insert('not cool value one');
+list.insert(3);
+
+list.includes(3); // this should return true because it is in the list
+list.includes(2);
+console.log('This is my constucted list:', list);
--- javascript/data-structures/linkedList/lib/linked-list.js
+++ javascript/data-structures/linkedList/lib/linked-list.js
@@ -18,11 +18,45 @@ class LinkedList{
     //new instance of the node constructor that I am bringing in
     this.head = node;
 
-    return insertValue;
+    return this.head;
   }
-  
 
-  //
+
+  // Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+
+  // includes method loops through the values that I have inserted
+  includes(findValue) {
+    let current = this.head;
+    // checking to see if the current node does not equal null
+    while (current != null) {
+
+      if(current.value === findValue) {
+        console.log(true);
+        return true;
+      }
+      current = current.next;
+    }
+    console.log(false);
+    return false;
+  }
+
+  // Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+  toString() {
+    let current = this.head;
+    let string = '';
+
+    while(current) {
+      if (current !== null) {
+        string = `${string} ${current.value}`;
+        current = current.next;
+      }
+      if (current === null) {
+        string = string + 'NULL';
+      }
+    }
+    console.log(string);
+  }
+
 }
 
 module.exports = LinkedList;
