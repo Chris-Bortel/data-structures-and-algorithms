096e7e7f7542c513444d464b6a9ac06810977f4b
Chris Bortel <bortelcc19@gmail.com>
2020-09-14 20:43:31 -0700

    Pop is not responding as expected

--- javascript/code-challenges/stacksAndQueues/__tests/stacks-and-queues.test.js
+++ javascript/code-challenges/stacksAndQueues/__tests/stacks-and-queues.test.js
@@ -2,11 +2,21 @@
 
 const Stack = require('../stacks-and-queues.js');
 
-// describe()
 it('can successfully push into a stack', () => {
   let stack = new Stack();
   stack.push(4);
+  console.log('line 7 +++++++++++', stack);
   expect(stack.top.value).toBe(4);
+});
+
+it('can successfully pop from a stack', () => {
+  let stack = new Stack();
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  stack.pop();
+  console.log(stack);
+  expect(stack.top.value).toBe(2);
 });
 
 
--- javascript/code-challenges/stacksAndQueues/stacks-and-queues.js
+++ javascript/code-challenges/stacksAndQueues/stacks-and-queues.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class Node {
-  constructor(val) {
-    this.value = val;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
@@ -13,15 +13,30 @@ class Stack {
     this.size = 0;
   }
 
-  push(val) {
-    let node = new Node(val);
+  push(value) {
+    let node = new Node(value);
     //creates new node
     if(this.top === null){
       this.top = node;
+    } else {
+      let oldTop = this.top;
+      node.next = oldTop;
     }
     this.size ++;
   }
 
+  // Pop is not functioning as expected
+  pop() {
+    if(this.size > 0) {
+      this.size --;
+      let top = this.top;
+      this.top = this.top.next;
+      return top.Value;
+
+    } else {
+      return 'exception';
+    }
+  }
 }
 
 module.exports = Stack;
